9251a930021f062b78b48cbf87c8211482ee4912
Victor <victor@my.home>
2021-08-25 15:58:12 +0300

    File Count All

---
+++ Files. Read. Count/Files. Read. Count/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Files.Read.Count
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Numbers numbers = new Numbers();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDirectoryDialogFind_Click(object sender, RoutedEventArgs e)
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(dialog.SelectedPath))
+                {
+                    SelectNeedFiles(dialog);
+                    numbers.threads = new System.Threading.Thread[numbers.files.Count];
+
+                    int i = 0;
+                    foreach (string path in numbers.files)
+                    {
+                        numbers.threads[i] = new Thread(numbers.ReadAllInfo);
+                        numbers.threads[i].Start(path);
+                        ++i;
+                    }
+
+                    for (int j = 0; j < numbers.threads.Length; j++)
+                    {
+                        numbers.threads[j].Join();
+                    }
+                    ShowAllInfo();
+                }
+            }
+        }
+
+        private void SelectNeedFiles(FolderBrowserDialog dialog)
+        {
+            foreach (string item in Directory.GetFiles(dialog.SelectedPath).ToList())
+            {
+                if (item.Contains(".txt"))
+                {
+                    numbers.files.Add(item);
+                }
+            }
+        }
+
+        private void ShowAllInfo()
+        {
+            lblShowInfo.Content = $"Words : {Numbers.Words}\nLines : {Numbers.Lines}\nPunctuation : {Numbers.Punctuation}";
+        }
+    }
+}
---
+++ Files. Read. Count/Files. Read. Count/Numbers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Files.Read.Count
+{
+    public class Numbers
+    {
+        public Thread[] threads = null;
+
+        public static int Words { get; set; }
+        public static int Lines { get; set; }
+        public static int Punctuation { get; set; }
+
+        public List<string> files { get; set; } = new List<string>();
+
+        public void ReadAllInfo(object obj)
+        {
+            string word = null;
+            foreach (char item in File.ReadAllText((string)obj))
+            {
+                lock (this)
+                {
+                    if (item >= (char)33 && item <= (char)47)
+                    {
+                        Punctuation++;
+                    }
+
+                    if (item == '\n')
+                    {
+                        Lines++;
+                    }
+
+                    if (item == ' ')
+                    {
+                        Words++;
+                        word = null;
+                    }
+                    word += item;
+                }
+            }
+        }
+    }
+}
